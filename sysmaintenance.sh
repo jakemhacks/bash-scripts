c4ed1440ea353ac3317fae9d9e6707d910b13461
Jake <jakemhacks@gmail.com>
2025-12-31 16:51:14 -0600

    Updated status messages

--- sysmaintenance.sh
+++ sysmaintenance.sh
@@ -5,21 +5,21 @@
 
 echo "===== System Maintainer ====="
 # update packages with pacman
-echo "Updating system packages..."
+echo "===== Updating system packages ====="
 sudo pacman -Syu --noconfirm
 
 # update packages from the AUR
-echo "Updating AUR packages..."
+echo "===== Updating AUR packages ====="
 yay --noconfirm
 
 # pacman does not remove old packages from cache.
 # this command removes the all but the last 3 versions of installed packages.
-echo "Cleaning package cache (keeping last 3 versions)..."
+echo "===== Cleaning package cache (keeping last 3 versions) ====="
 sudo paccache -rvk 3
 
 # this command searches for and removes any dependencies
 # that were installed previously and are no longer utilized.
-echo "Removing orphaned packages..."
+echo "===== Removing orphaned packages ====="
 orphans=$(pacman -Qtdq)
 if [ -n "$orphans" ]; then
   sudo pacman -Rns "$orphans" --noconfirm
@@ -27,7 +27,7 @@ else
   echo "No orphaned packages found."
 fi
 
-echo "Checking for failed systemd services..."
+echo "===== Checking for failed systemd services ====="
 systemctl --failed
 
 echo "Disk Usage:"
